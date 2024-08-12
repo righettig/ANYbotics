42fbb805d94cb5621b856b70ab14e51dfa742f91
Giacomo Righetti <righetti.giacomo@live.com>
2024-08-12 12:01:34 +0200

    Made Id and Name mandatory properties

--- anybotics-anymal-api/Models/AgentDto.cs
+++ anybotics-anymal-api/Models/AgentDto.cs
@@ -4,8 +4,8 @@ namespace anybotics_anymal_api.Models;
 
 public class AgentDto
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
+    public required string Id { get; set; }
+    public required string Name { get; set; }
     public int BatteryLevel { get; set; }
     public Status Status { get; set; }
 }
