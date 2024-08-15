34bf9903ba00a5f602f42a432ecf39f798fe3b8a
Giacomo Righetti <righetti.giacomo@live.com>
2024-08-15 09:48:43 +0200

    Improved login component

--- anybotics-workforce-ng/src/app/login/login.component.ts
+++ anybotics-workforce-ng/src/app/login/login.component.ts
@@ -1,12 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
@@ -14,12 +26,31 @@ export class LoginComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
 
+  // Predefined users for quick selection
+  users = [
+    { email: 'giacomo@anybotics.com', password: 'q1w2e3' },
+    { email: 'guest@anybotics.com', password: 'qwerty123' },
+    { email: 'admin@anybotics.com', password: 'q1w2e3' },
+  ];
+
   email = '';
   password = '';
+
+  showOrHidePassword = signal(true);
 
   login() {
     this.authService.login(this.email, this.password).then(() => {
       this.router.navigate(['/agents']);
     });
+  }
+
+  onUserSelect(user: { email: string; password: string }) {
+    this.email = user.email;
+    this.password = user.password;
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.showOrHidePassword.set(!this.showOrHidePassword());
+    event.stopPropagation();
   }
 }
