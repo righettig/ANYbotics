6bdf91983df043278c5177e9280344e7043cf2a9
Giacomo Righetti <righetti.giacomo@live.com>
2024-08-15 09:56:45 +0200

    Improved visuals

--- anybotics-workforce-ng/src/app/user-profile/user-profile.component.ts
+++ anybotics-workforce-ng/src/app/user-profile/user-profile.component.ts
@@ -1,25 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../services/auth.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [MatIcon, MatMenuModule],
+  imports: [MatIcon, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
   private router = inject(Router);
-  
+
   isLoggedIn = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService.isLoggedIn.subscribe(status => this.isLoggedIn = status);
+    this.authService.isLoggedIn.subscribe(
+      (status) => (this.isLoggedIn = status)
+    );
   }
 
   onLogout() {
