e5ad1e07ce34410935d38e41086941aab7eb4c20
Giacomo Righetti <righetti.giacomo@live.com>
2024-08-28 17:12:17 +0200

    Fixed language type for typescript; improved snippets

--- anybotics.bots/src/behaviours/behaviours.controller.ts
+++ anybotics.bots/src/behaviours/behaviours.controller.ts
@@ -12,33 +12,33 @@ const predefined: BehaviourSnippet[] = [
         version: 'v1',
         language: 'csharp',
         code: `
-            public class Bot
-            {
-                public ICommand Update(IWorldState worldState)
-                {
-                    // Return an appropriate ICommand instance
-                }
-            }
+public class Bot
+{
+    public ICommand Update(IWorldState worldState)
+    {
+        // Return an appropriate ICommand instance
+    }
+}
         `,
     },
     {
         version: 'v1',
         language: 'python',
         code: `
-            class Bot:
-                def update(self, world_state: IWorldState) -> ICommand:
-                    # Return an appropriate ICommand instance
+class Bot:
+    def update(self, world_state: IWorldState) -> ICommand:
+        # Return an appropriate ICommand instance
         `,
     },
     {
         version: 'v1',
-        language: 'ts',
+        language: 'typescript',
         code: `
-            class Bot {
-                update(worldState: IWorldState): ICommand {
-                    // Return an appropriate ICommand instance
-                }
-            }
+class Bot {
+    update(worldState: IWorldState): ICommand {
+        // Return an appropriate ICommand instance
+    }
+}
         `,
     }
 ];
--- anybotics.bots/src/bots/bot.schema.ts
+++ anybotics.bots/src/bots/bot.schema.ts
@@ -2,11 +2,11 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type BotDocument = Bot & Document;
-export type BotLanguage = 'csharp' | 'python' | 'ts';
+export type BotLanguage = 'csharp' | 'python' | 'typescript';
 
 @Schema()
 export class BotBehaviour {
-    @Prop({ required: true, enum: ['csharp', 'python', 'ts'] })
+    @Prop({ required: true, enum: ['csharp', 'python', 'typescript'] })
     language: BotLanguage;
 
     @Prop({ required: true })
