f2b7e8c96f5e19aa1cc10e1795b5bf4125404861
Giacomo Righetti <righetti.giacomo@live.com>
2024-08-31 20:55:48 +0200

    Added rate limit example (bots)

--- anybotics.bots/src/app.module.ts
+++ anybotics.bots/src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BotsModule } from './bots/bots.module';
 import { BehavioursModule } from './behaviours/behaviours.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL!),
+    ThrottlerModule.forRoot([{
+      ttl: 60 * 1000,
+      limit: 10,
+    }]),
     BotsModule, BehavioursModule,
   ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }    
+  ]
 })
 export class AppModule { }
